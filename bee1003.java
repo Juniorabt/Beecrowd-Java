08ccca1c32f75e01111299f7c6e0f207dd590453
Edioelson Júnior A. B. Teixeira <129232498+Juniorabt@users.noreply.github.com>
2023-09-27 15:52:49 -0300

    Bee1003 - Simple sum

---
+++ bee1003.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bee1003{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, sum;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        sum = A + B;
+
+        System.out.println("SUM = " + sum);
+
+        sc.close();
+
+    }
+}
